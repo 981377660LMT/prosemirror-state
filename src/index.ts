f4e2d81c6fdffe93fbc18bc0feedbbf1f265273f
liaomingtian <liaomingtian@bytedance.com>
2025-08-29 01:49:07 +0800

    格式化导入语句和导出语句，提升代码可读性

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-export {Selection, SelectionRange, TextSelection, NodeSelection, AllSelection, SelectionBookmark} from "./selection"
+export {
+  Selection,
+  SelectionRange,
+  TextSelection,
+  NodeSelection,
+  AllSelection,
+  SelectionBookmark
+} from './selection'
 
-export {Transaction, Command} from "./transaction"
+export { Transaction, Command } from './transaction'
 
-export {EditorState, EditorStateConfig} from "./state"
+export { EditorState, EditorStateConfig } from './state'
 
-export {Plugin, PluginKey, PluginSpec, StateField, PluginView} from "./plugin"
+export { Plugin, PluginKey, PluginSpec, StateField, PluginView } from './plugin'
--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,6 @@
-import {type EditorView, type EditorProps} from "prosemirror-view"
-import {EditorState, EditorStateConfig} from "./state"
-import {Transaction} from "./transaction"
+import { type EditorView, type EditorProps } from 'prosemirror-view'
+import { EditorState, EditorStateConfig } from './state'
+import { Transaction } from './transaction'
 
 /// This is the type passed to the [`Plugin`](#state.Plugin)
 /// constructor. It provides a definition for a plugin.
@@ -37,7 +37,11 @@ export interface PluginSpec<PluginState> {
   /// with the new state and new transactions—but only the new
   /// transactions, i.e. it won't be passed transactions that it
   /// already saw.
-  appendTransaction?: (transactions: readonly Transaction[], oldState: EditorState, newState: EditorState) => Transaction | null | undefined
+  appendTransaction?: (
+    transactions: readonly Transaction[],
+    oldState: EditorState,
+    newState: EditorState
+  ) => Transaction | null | undefined
 
   /// Additional properties are allowed on plugin specs, which can be
   /// read via [`Plugin.spec`](#state.Plugin.spec).
@@ -55,11 +59,11 @@ export type PluginView = {
   destroy?: () => void
 }
 
-function bindProps(obj: {[prop: string]: any}, self: any, target: {[prop: string]: any}) {
+function bindProps(obj: { [prop: string]: any }, self: any, target: { [prop: string]: any }) {
   for (let prop in obj) {
     let val = obj[prop]
     if (val instanceof Function) val = val.bind(self)
-    else if (prop == "handleDOMEvents") val = bindProps(val, self, {})
+    else if (prop == 'handleDOMEvents') val = bindProps(val, self, {})
     target[prop] = val
   }
   return target
@@ -75,7 +79,7 @@ export class Plugin<PluginState = any> {
     readonly spec: PluginSpec<PluginState>
   ) {
     if (spec.props) bindProps(spec.props, this, this.props)
-    this.key = spec.key ? spec.key.key : createKey("plugin")
+    this.key = spec.key ? spec.key.key : createKey('plugin')
   }
 
   /// The [props](#view.EditorProps) exported by this plugin.
@@ -85,7 +89,9 @@ export class Plugin<PluginState = any> {
   key: string
 
   /// Extract the plugin's state field from an editor state.
-  getState(state: EditorState): PluginState | undefined { return (state as any)[this.key] }
+  getState(state: EditorState): PluginState | undefined {
+    return (state as any)[this.key]
+  }
 }
 
 /// A plugin spec may provide a state field (under its
@@ -117,9 +123,9 @@ export interface StateField<T> {
 const keys = Object.create(null)
 
 function createKey(name: string) {
-  if (name in keys) return name + "$" + ++keys[name]
+  if (name in keys) return name + '$' + ++keys[name]
   keys[name] = 0
-  return name + "$"
+  return name + '$'
 }
 
 /// A key is used to [tag](#state.PluginSpec.key) plugins in a way
@@ -131,12 +137,18 @@ export class PluginKey<PluginState = any> {
   key: string
 
   /// Create a plugin key.
-  constructor(name = "key") { this.key = createKey(name) }
+  constructor(name = 'key') {
+    this.key = createKey(name)
+  }
 
   /// Get the active plugin with this key, if any, from an editor
   /// state.
-  get(state: EditorState): Plugin<PluginState> | undefined { return state.config.pluginsByKey[this.key] }
+  get(state: EditorState): Plugin<PluginState> | undefined {
+    return state.config.pluginsByKey[this.key]
+  }
 
   /// Get the plugin's state from an editor state.
-  getState(state: EditorState): PluginState | undefined { return (state as any)[this.key] }
+  getState(state: EditorState): PluginState | undefined {
+    return (state as any)[this.key]
+  }
 }
